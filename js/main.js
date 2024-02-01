b716b53e69567a80bb05471a655e4b42ccecdf69
kevinDronnn <voroshilin777@icloud.com>
2024-02-02 00:31:06 +0200

    added settings modal window

--- js/main.js
+++ js/main.js
@@ -1,31 +1,79 @@
-// Function to open the modal
+
 function openModal() {
   var modal = document.getElementById("myModal");
   modal.style.display = "block";
 
-  // Get the modal content by class name
+
   var modalContent = document.querySelector(".modal-content");
   modalContent.style.display = "block";
 }
 
-// Function to close the modal
+
 function closeModal() {
   var modal = document.getElementById("myModal");
   modal.style.display = "none";
 
-  // Get the modal content by class name
+
   var modalContent = document.querySelector(".modal-content");
   modalContent.style.display = "none";
 }
 
-// Function to close the modal if clicked outside the modal content
+
 window.onclick = function (event) {
   var modal = document.getElementById("myModal");
   if (event.target == modal) {
     modal.style.display = "none";
 
-    // Get the modal content by class name
+
     var modalContent = document.querySelector(".modal-content");
     modalContent.style.display = "none";
   }
 };
+
+function showVideo() {
+  hideAllLabelPairs();
+  document
+    .querySelectorAll(".label-select-pair-video")
+    .forEach(function (element) {
+      element.style.display = "flex";
+    });
+  adjustModalHeight(200); 
+}
+
+function showAudio() {
+  hideAllLabelPairs();
+  document
+    .querySelectorAll(".label-select-pair-audio")
+    .forEach(function (element) {
+      element.style.display = "flex";
+    });
+  adjustModalHeight(300);
+}
+
+function hideAllLabelPairs() {
+  document.querySelectorAll(".label-select-pair").forEach(function (element) {
+    element.style.display = "none";
+  });
+  document
+    .querySelectorAll(".label-select-pair-video")
+    .forEach(function (element) {
+      element.style.display = "none";
+    });
+  document
+    .querySelectorAll(".label-select-pair-audio")
+    .forEach(function (element) {
+      element.style.display = "none";
+    });
+}
+
+function adjustModalHeight(height) {
+  var modalContent = document.querySelector(".modal-content");
+  modalContent.style.height = height + "px";
+}
+
+function updateVolume(value) {
+  document.getElementById("volumeValue").innerText = value + "%";
+}
+
+
+showVideo();
